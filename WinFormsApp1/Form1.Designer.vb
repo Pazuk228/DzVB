cd6c23e59fa91aafc0065e4eb8c4d7d6993608ca
Pazuk228 <nusha0926@gmail.com>
2026-09-11 16:53:52 +0300

    программа 1

--- WinFormsApp1/Form1.Designer.vb
+++ WinFormsApp1/Form1.Designer.vb
@@ -22,10 +22,123 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Label1 = New Label()
+        Button1 = New Button()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(375, 79)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.DarkSeaGreen
+        Button1.Font = New Font("Segoe UI Variable Small Semibol", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Button1.Location = New Point(61, 343)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(121, 41)
+        Button1.TabIndex = 1
+        Button1.Text = "Начать"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(375, 119)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Label2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(375, 164)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "Label3"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(375, 208)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(41, 15)
+        Label4.TabIndex = 4
+        Label4.Text = "Label4"
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.DarkSeaGreen
+        Button2.Font = New Font("Segoe UI Variable Small Semibol", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Button2.Location = New Point(233, 343)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(121, 41)
+        Button2.TabIndex = 5
+        Button2.Text = "Очистить"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.DarkSeaGreen
+        Button3.Font = New Font("Segoe UI Variable Small Semibol", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Button3.Location = New Point(409, 343)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(121, 41)
+        Button3.TabIndex = 6
+        Button3.Text = "Цвет"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.DarkSeaGreen
+        Button4.Font = New Font("Segoe UI Variable Small Semibol", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(204))
+        Button4.Location = New Point(602, 343)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(121, 41)
+        Button4.TabIndex = 7
+        Button4.Text = "Выход"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DarkGreen
         ClientSize = New Size(800, 450)
-        Text = "Form1"
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Первый проект Воронова"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
 
 End Class
--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -1,3 +1,30 @@
 ﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "ПМИ-б-о-25-1"
+        Label2.Text = "Воронова"
+        Label3.Text = "Дана"
+        Label4.Text = "Вячеславовна"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = ""
+        Label2.Text = ""
+        Label3.Text = ""
+        Label4.Text = ""
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Label1.ForeColor = Color.Red
+        Label2.ForeColor = Color.Red
+        Label3.ForeColor = Color.Red
+        Label4.ForeColor = Color.Red
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        End
+    End Sub
 End Class
